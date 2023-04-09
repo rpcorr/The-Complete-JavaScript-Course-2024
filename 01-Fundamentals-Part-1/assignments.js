405aab2b3528817f67b23f0ae7a26b578c70b6c0
Ronan Corr <ronan_corr@hotmail.com>
2023-04-09 11:21:55 -0400

    assignment #1 Values and Variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,12 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = 'Canada';
+let continent = 'North America';
+let population = 38654702;
+
+console.log(
+  `${country} is a part of ${continent} and has a population of ${population}`
+);
