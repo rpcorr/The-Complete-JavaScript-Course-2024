6c7badd7fcc2365475cb16a855521de16bd24f36
Ronan Corr <ronan_corr@hotmail.com>
2023-04-09 10:59:48 -0400

    working with values and variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,28 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Ronan');
+console.log(23);
+
+let firstName = 'Ronan';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let ronan_melissa = 'RM';
+let $function = 27;
+
+let person = 'Ronan';
+// all uppercase = a constant
+let PI = 3.1415;
+
+// excellent variable names
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+// poor variable names
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
