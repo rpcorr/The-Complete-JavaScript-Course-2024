6f92a7a7a36bf6b0e66be57a347a5681502188b1
Ronan Corr <ronan_corr@hotmail.com>
2023-04-09 12:12:10 -0400

    working with data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -10,19 +11,38 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let ronan_melissa = 'RM';
 let $function = 27;
 
 let person = 'Ronan';
-// all uppercase = a constant
 let PI = 3.1415;
 
-// excellent variable names
 let myFirstJob = 'Coder';
 let myCurrentJob = 'Teacher';
 
-// poor variable names
 let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Ronan');
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
