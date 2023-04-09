f8ed96021f213e0460d99a44d842a332e0aa862a
Ronan Corr <ronan_corr@hotmail.com>
2023-04-09 13:00:30 -0400

    working with basic operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -50,6 +50,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 // Lesson: let, const, var;
 let age = 30;
 age = 31;
@@ -64,3 +65,36 @@ job = 'teacher';
 
 lastName = 'Corr';
 console.log(lastName);
+*/
+
+// Lesson: Basic Operators
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Ronan';
+const lastName = 'Corr';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 == 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+// Comparision operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
