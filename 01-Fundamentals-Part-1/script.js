636f7c87c7738604391999ff547850c7c8d746ae
Ronan Corr <ronan_corr@hotmail.com>
2023-04-12 22:43:16 -0400

    working with operator precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -67,6 +67,7 @@ lastName = 'Corr';
 console.log(lastName);
 */
 
+/*
 // Lesson: Basic Operators
 
 // Math operators
@@ -98,3 +99,21 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+// Lesson: Operator Precedence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+//console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
