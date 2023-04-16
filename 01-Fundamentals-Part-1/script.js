c2440d08cc2ae7d33f92d122e3ca95582f2f6ba0
Ronan Corr <ronan_corr@hotmail.com>
2023-04-16 17:15:17 -0400

    working with  taking decisions - if/else statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -119,6 +119,7 @@ console.log(x, y);
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);*/
 
+/*
 // Lesson: Strings and Template Literals
 
 const firstName = 'Ronan';
@@ -145,4 +146,25 @@ lines'
 
 console.log(`String
 multiple
-lines`);
+lines`); */
+
+// Lesson: Taking Decisions: if/else Statements
+
+const age = 15;
+
+if (age >= 18) {
+  console.log('Sarah can start her driving license 🚗');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
