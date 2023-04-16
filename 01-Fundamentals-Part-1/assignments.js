80064d18f4e47f7ddade12eb53ae10fd1f586edc
Ronan Corr <ronan_corr@hotmail.com>
2023-04-16 08:17:28 -0400

    remove script file; change title; add declaration text

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,4 +1,5 @@
 // LECTURE: Values and Variables
+// Assignment 1
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
