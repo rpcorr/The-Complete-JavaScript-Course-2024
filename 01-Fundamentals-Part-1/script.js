afea9d8b8a510fc816fc9a24563380e142964e26
Ronan Corr <ronan_corr@hotmail.com>
2023-04-19 16:28:17 -0400

    working with truthy and falsy values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -172,6 +172,7 @@ console.log(century);
 
 */
 
+/*
 // Lesson: Type Conversion and Coercion
 
 // type conversion
@@ -198,3 +199,26 @@ console.log(n); // result 10 number
 
 console.log(2 + 3 + 4 + '5'); // outputs '95' (string)
 console.log('10' - '4' - '3' - 2 + '5'); // outputs '15' (string)
+*/
+
+// Lesson: Truthy and Falsy Values
+
+// There are 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Ronan'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
