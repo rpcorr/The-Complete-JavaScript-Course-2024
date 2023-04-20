ac756e3f2b2f3567156519d745cd6fadc625f1c7
Ronan Corr <ronan_corr@hotmail.com>
2023-04-20 04:02:35 -0400

    working with logical operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -225,6 +225,7 @@ if (height) {
 }
 */
 
+/*
 // Lesson: Equality Operators: == vs ===
 const age = '18';
 if (age === 18) console.log('You just became an adult 😀 (strict)');
@@ -247,3 +248,27 @@ if (favourite === 17) {
 }
 
 if (favourite !== 17) console.log('Why not 17?');
+*/
+
+// Lesson: Logical Operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+/*if (hasDriversLicense && hasGoodVision) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}*/
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
