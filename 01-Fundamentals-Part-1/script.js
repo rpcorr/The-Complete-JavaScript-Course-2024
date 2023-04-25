18b39e9ed8508c3d626f5f24ff3731277766dddb
Ronan Corr <ronan_corr@hotmail.com>
2023-04-25 09:11:38 -0400

    working with the conditional ternary operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -276,6 +276,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 }
 */
 
+/*
 // Lesson: The Switch Statement
 const day = 'sunday';
 
@@ -318,3 +319,25 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
+*/
+
+// Lesson: The Conditional (Ternary) Operator
+const age = 18;
+/*age >= 18
+  ? console.log('I like to drink wine 🍷')
+  : console.log('I like to drink water 💧');
+*/
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
