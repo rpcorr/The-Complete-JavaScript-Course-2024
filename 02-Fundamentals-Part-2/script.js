3f4d9ac8886df514706b22e11aca1081954655b9
Ronan Corr <ronan_corr@hotmail.com>
2023-05-02 07:52:39 -0400

    working with function declarations vs expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -10,6 +10,7 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const interface = 'Audio';
 // const private = 534; */
 
+/*
 // Lesson Functions
 function logger() {
   console.log('My name is Ronan');
@@ -31,4 +32,22 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
-const num = Number('23');
+const num = Number('23'); */
+
+// Lesson: Function Declarations vs. Expressions
+
+// function declaration
+const age1 = calcAge1(1991);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+// function expression (annoymous function)
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
