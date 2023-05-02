69de357e9a6858319f078849a5ddb493a8a382d9
Ronan Corr <ronan_corr@hotmail.com>
2023-05-02 13:55:23 -0400

    reviewing functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -72,6 +72,7 @@ console.log(yearsUntilRetirement(1977, 'Ronan'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
 */
 
+/*
 // Lesson: Functions Calling Other Functions
 function cutFruitPieces(fruit) {
   return fruit * 4;
@@ -85,3 +86,25 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
+*/
+
+// Lesson: Reviewing Functions
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, 'Ronan'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
