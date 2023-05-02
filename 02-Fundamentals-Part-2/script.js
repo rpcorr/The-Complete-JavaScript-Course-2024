a9cdc1a07ae36d4f1f2be1ae11a4796c3fef9f31
Ronan Corr <ronan_corr@hotmail.com>
2023-05-02 09:27:21 -0400

    working with arrow functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -34,20 +34,38 @@ console.log(appleOrangeJuice);
 
 const num = Number('23'); */
 
+/*
 // Lesson: Function Declarations vs. Expressions
 
-// function declaration
+// Function declaration
 const age1 = calcAge1(1991);
 
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 }
 
-// function expression (annoymous function)
+// Function expression (annoymous function)
 const calcAge2 = function (birthYear) {
   return 2037 - birthYear;
 };
 
 const age2 = calcAge2(1991);
 
-console.log(age1, age2);
+console.log(age1, age2); */
+
+// Lesson: Arrow Functions
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1977, 'Ronan'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
