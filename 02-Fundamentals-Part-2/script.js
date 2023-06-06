f50b4c9ecd356aa8756ac094d640836a13f5debc
Ronan Corr <ronan_corr@hotmail.com>
2023-06-06 11:18:36 -0400

    working with looping backwards and loops in loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -327,6 +327,7 @@ for (let rep = 1; rep <= 10; rep++) {
 }
 */
 
+/*
 // Lesson: Looping Arrays, Breaking and Continuing
 const jonas = [
   'Jonas',
@@ -377,4 +378,30 @@ for (let i = 0; i < jonas.length; i++) {
   if (typeof jonas[i] === 'number') break;
 
   console.log(jonas[i], typeof jonas[i]);
+}
+
+*/
+
+// Lesson: Looping Backwards and Loops in Loops
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'Teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`--------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♂️`);
+  }
 }
