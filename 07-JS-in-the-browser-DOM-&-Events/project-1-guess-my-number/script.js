87f5c98758f405626d140d5fa620ecbd6f3e0e57
Ronan Corr <ronan_corr@hotmail.com>
2023-06-07 13:55:36 -0400

    change function to an arrow function

--- 07-JS-in-the-browser-DOM-&-Events/project-1-guess-my-number/script.js
+++ 07-JS-in-the-browser-DOM-&-Events/project-1-guess-my-number/script.js
@@ -4,7 +4,7 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-const displayMessage = function (message) {
+const displayMessage = (message) => {
   document.querySelector('.message').textContent = message;
 };
 
