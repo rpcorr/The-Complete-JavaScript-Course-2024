0813b0d0e006981788cdc4cd0bfe3cbf45cfb7c0
Ronan Corr <ronan_corr@hotmail.com>
2023-06-08 16:23:45 -0400

    open and close modal via adding event listeners to buttons to add or remove .hidden class

--- 07-JS-in-the-browser-DOM-&-Events/project-2-modal-window/script.js
+++ 07-JS-in-the-browser-DOM-&-Events/project-2-modal-window/script.js
@@ -3,9 +3,20 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsShowModal);
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
