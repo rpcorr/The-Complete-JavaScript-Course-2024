046a62686a43629d61a72731158cc4ffec82af28
Ronan Corr <ronan_corr@hotmail.com>
2023-06-08 16:43:05 -0400

    have the ESC key close the modal as well

--- 07-JS-in-the-browser-DOM-&-Events/project-2-modal-window/script.js
+++ 07-JS-in-the-browser-DOM-&-Events/project-2-modal-window/script.js
@@ -20,3 +20,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// add ESC key functionally
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
