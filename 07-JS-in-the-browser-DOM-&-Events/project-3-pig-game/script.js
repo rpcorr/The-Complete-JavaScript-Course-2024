d1ef51b4d69d988208f18e95fd140001b5d69c4a
Ronan Corr <ronan_corr@hotmail.com>
2023-06-09 12:09:05 -0400

    when a player wins the game add player-winner class and remove player--active class

--- 07-JS-in-the-browser-DOM-&-Events/project-3-pig-game/script.js
+++ 07-JS-in-the-browser-DOM-&-Events/project-3-pig-game/script.js
@@ -59,8 +59,16 @@ btnHold.addEventListener('click', () => {
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
   // 2. Check if player's score is >=100
-  // Finish the game
-
-  // Switch to the next player
-  switchPlayer();
+  if (scores[activePlayer] >= 20) {
+    // Finish the game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    // Switch to the next player
+    switchPlayer();
+  }
 });
