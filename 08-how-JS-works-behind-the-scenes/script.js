bdc919dba216fbad6aedebe9f4d3d454e68ac518
Ronan Corr <ronan_corr@hotmail.com>
2023-06-11 13:35:52 -0400

    hoisting and the dead zone in practice

--- 08-how-JS-works-behind-the-scenes/script.js
+++ 08-how-JS-works-behind-the-scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+/*
+// Scoping in practice
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -36,3 +37,49 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age); // reference error (not accessible)
 // printAge(); // reference error (not accessible)
+*/
+
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job); // in TDZ
+// console.log(year); // in TDZ
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDeclaration(2, 3)); // works
+// console.log(addExpression(2, 3)); // in the TDZ
+// console.log(addArrowExpression); // doesn't work when declared with var
+// console.log(addArrowExpression(2, 3)); // in the TDZ
+
+function addDeclaration(a, b) {
+  return a + b;
+}
+
+const addExpression = function (a, b) {
+  return a + b;
+};
+
+var addArrowExpression = (a, b) => a + b;
+
+// Example
+console.log(numProduct);
+if (!numProduct) deleteShoppingCart();
+
+var numProduct = 10;
+
+function deleteShoppingCart() {
+  console.log('All producted deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
