59e26ee5066fd7773688792a6176576c78c60d8f
Ronan Corr <ronan_corr@hotmail.com>
2023-06-12 09:49:57 -0400

    working with destructuring arrays

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -26,4 +26,52 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+// Lesson: Destructuring Arrays
+const arr = [2, 3, 4];
+
+// assigning the three variables without destructuring
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// assigning the three variables using destructuring
+const [x, y, z] = arr; // destructing an array
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables (previous way)
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(`Previous way: ${main}, ${secondary}`);
+
+// Using destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Retrieve 2 return values fron a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
