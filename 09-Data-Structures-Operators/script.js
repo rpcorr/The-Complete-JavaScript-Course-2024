834da5408a3b147d5554c1198f385298d236f616
Ronan Corr <ronan_corr@hotmail.com>
2023-06-12 15:39:42 -0400

    working with logical assignment operators

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -273,6 +273,7 @@ restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 */
 
+/*
 // Lesson: The Nullish Coalescing Operator (??)
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
@@ -281,3 +282,39 @@ console.log(guests);
 // Nullish values are: null and undefined (doesn't include 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
+*/
+
+// Lesson: Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  //numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// - result the same as above
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator ( null or undefined )
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
