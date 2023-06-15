35bee2ab5290ead996f03c917e45d43e30954f54
Ronan Corr <ronan_corr@hotmail.com>
2023-06-15 15:55:13 -0400

    working with Optional Chaining (?.)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -346,3 +346,34 @@ for (const [i, el] of menu.entries()) {
 
 // console.log([...menu.entries()]);
 */
+
+// Lesson Optional Chaining(?.)
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example of optional chaining operator and the nullish coalescing operator working together.
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+console.log(users[0]?.name ?? 'User array empty');
+
+// the above is the short form to without optional chaining
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
