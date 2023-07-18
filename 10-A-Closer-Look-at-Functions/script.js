62fb73561005664277c0cb82f2946f1e6cc4a3ad
Ronan Corr <ronan_corr@hotmail.com>
2023-07-18 17:26:23 -0400

    working with default parameters

--- 10-A-Closer-Look-at-Functions/script.js
+++ 10-A-Closer-Look-at-Functions/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// Lesson: Default Parameters
+
+const bookings = [];
+
+const createBooking = function (
+  fligtNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    fligtNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH456', 2);
+createBooking('LH456', 5);
+createBooking('LH456', undefined, 1000);
