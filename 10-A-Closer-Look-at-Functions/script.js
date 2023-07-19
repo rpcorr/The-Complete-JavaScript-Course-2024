fd8c88e62431e3bbe7b755393e0ee53aca676940
Ronan Corr <ronan_corr@hotmail.com>
2023-07-19 16:02:06 -0400

    working with higher-order function

--- 10-A-Closer-Look-at-Functions/script.js
+++ 10-A-Closer-Look-at-Functions/script.js
@@ -30,6 +30,7 @@ createBooking('LH456', 5);
 createBooking('LH456', undefined, 1000);
 */
 
+/*
 // Lesson: How Passing Arguments Works: Value vs. Reference
 const flight = 'LH234';
 const jonas = {
@@ -62,3 +63,33 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+// Lesson: Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋🏼');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
