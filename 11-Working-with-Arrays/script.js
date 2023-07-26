feea04ea4b768f15bbb782967651af7cedd7d6e1
Ronan Corr <ronan_corr@hotmail.com>
2023-07-26 11:55:01 -0400

    working with forEach With Maps and Set

--- 11-Working-with-Arrays/script.js
+++ 11-Working-with-Arrays/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -133,6 +127,7 @@ console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
 */
 
+/*
 // Lesson: Looping Arrays: forEach
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -155,4 +150,25 @@ movements.forEach(function (movement, index, array) {
   } else {
     console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
   }
+});
+*/
+
+// Lesson: forEach With Maps and Sets
+
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
 });
