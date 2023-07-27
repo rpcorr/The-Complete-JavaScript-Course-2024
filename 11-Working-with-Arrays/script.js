2db8a9903e3b2c79abd16a6513498e168493ba73
Ronan Corr <ronan_corr@hotmail.com>
2023-07-27 11:53:14 -0400

    using the map method

--- 11-Working-with-Arrays/script.js
+++ 11-Working-with-Arrays/script.js
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -193,3 +193,29 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+// Lesson: The map Method
+const euroToUSD = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * euroToUSD;
+// });
+
+const movementsUSD = movements.map(mov => mov * euroToUSD);
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * euroToUSD);
+
+// console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
