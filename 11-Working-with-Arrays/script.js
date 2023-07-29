74b51d9f5bbfb35e4ee4852f7f818645d795cd30
Ronan Corr <ronan_corr@hotmail.com>
2023-07-29 04:38:11 -0400

    using some and every array methods

--- 11-Working-with-Arrays/script.js
+++ 11-Working-with-Arrays/script.js
@@ -191,6 +191,22 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -431,3 +447,25 @@ console.log(accounts);
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
 */
+
+// Lesson: some and every
+console.log(movements);
+
+// Check for EQUALITY
+console.log(movements.includes(-130));
+
+// Check for SOME: CONDITION
+console.log(movements.some(mov => mov === -130)); // same as line 439
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// Check for EVERY: CONDITION
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
