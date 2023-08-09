559f51a438660cb48080a2369e9c6e89de92396f
Ronan Corr <ronan_corr@hotmail.com>
2023-08-09 14:50:05 -0400

    working with the Remainder Operator

--- 12-Numbers-Dates-Intl-and-Timers/script.js
+++ 12-Numbers-Dates-Intl-and-Timers/script.js
@@ -294,6 +294,7 @@ console.log(Number.isInteger('23.0')); // false
 console.log(Number.isInteger(23 / 0)); // false
 */
 
+/*
 // Lesson: Math and Rounding
 
 // square root
@@ -348,3 +349,34 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+*/
+
+// Lesson: The Remainder Operator
+
+console.log(5 % 2); // 1
+console.log(5 / 2); // 2.5 // 5 = 2 * 2 + 1
+console.log(8 % 2); // 0
+console.log(8 % 3); // 2
+console.log(8 / 3); // 2.666666665 // 8 = 2 * 3 + 2
+
+console.log(6 % 2); // 0
+console.log(6 / 2); // 3
+
+console.log(7 % 2); // 1
+console.log(7 / 2); // 3.5
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6 ...
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+
+    // 0, 3, 6, 9 ...
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
