2883efd015c4e7ea398fbd0fd5c6be700975971a
Ronan Corr <ronan_corr@hotmail.com>
2023-08-14 11:34:35 -0400

    working with Timers: setTimeout and setInterval

--- 12-Numbers-Dates-Intl-and-Timers/script.js
+++ 12-Numbers-Dates-Intl-and-Timers/script.js
@@ -300,14 +300,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -602,6 +604,7 @@ const days1 = calcDaysPassed(
 console.log(days1);
 */
 
+/*
 // Lesson: Internationalizing Numbers (Intl)
 
 const num = 3884764.23;
@@ -623,3 +626,25 @@ console.log(
   navigator.language,
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
+*/
+
+// Lesson: Timers: setTimeout and setInterval
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) =>
+    console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕.`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+  console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+}, 1000);
