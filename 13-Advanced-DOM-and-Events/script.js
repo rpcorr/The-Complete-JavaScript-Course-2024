97a00b5bed5c9cf6d15ff42112255f159bc25755
Ronan Corr <ronan_corr@hotmail.com>
2023-08-18 11:10:34 -0400

    working with Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-and-Events/script.js
+++ 13-Advanced-DOM-and-Events/script.js
@@ -32,3 +32,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+// Lesson: Selecting, Creating, and Deleting Elements
+
+// Selection elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Create and inserting elements
+// .interAdjacentHMTL
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent =
+//  'We use cookies for improved functionality and analytics.';
+
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message); // the old way
+  });
