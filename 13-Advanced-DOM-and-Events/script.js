403d63772f623e926762e139a9d83731e5fc2abf
Ronan Corr <ronan_corr@hotmail.com>
2023-08-23 15:38:12 -0400

    Implementing a sticky navigation: The Scroll Event

--- 13-Advanced-DOM-and-Events/script.js
+++ 13-Advanced-DOM-and-Events/script.js
@@ -155,6 +155,18 @@ nav.addEventListener('mouseout', function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+///////////////////////////////////////
+// Sticky Navigation: The Scroll Event
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function (e) {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
