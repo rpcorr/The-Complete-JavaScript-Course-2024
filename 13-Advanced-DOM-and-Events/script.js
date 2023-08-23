36235e242824d8cb758c2555512d5e753dcb522b
Ronan Corr <ronan_corr@hotmail.com>
2023-08-23 12:37:57 -0400

    Building a Tabbed Component

--- 13-Advanced-DOM-and-Events/script.js
+++ 13-Advanced-DOM-and-Events/script.js
@@ -99,6 +99,33 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+///////////////////////////////////////
+// Building a Tabbed Component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // Activate tab
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked;
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
