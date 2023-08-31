721a6b4383fd64ca836f45b6eadd31e5baccecc3
Ronan Corr <ronan_corr@hotmail.com>
2023-08-31 16:34:09 -0400

    Inheritance Between "Classes": Constructor Functions lecture

--- 14-Object-Oriented-Programming-OOP-with-JS/script.js
+++ 14-Object-Oriented-Programming-OOP-with-JS/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Lesson: Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
   // Instance properties
@@ -194,3 +194,43 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
+
+*/
+
+// Lesson: Inheritance Between "Classes": Constructor Functions
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Student.prototype inheriting from Person.prototype (Linking prototypes)
+Student.prototype = Object.create(Person.prototype);
+// Student.prototype = Person.prototype doesn't work
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student); // true
+console.log(mike instanceof Person); // true
+console.log(mike instanceof Object); // true
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
