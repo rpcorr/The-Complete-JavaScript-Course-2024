33a90c841be5f8a4dbc6143843e18e1f7d61fd20
Ronan Corr <ronan_corr@hotmail.com>
2023-08-31 08:00:38 -0400

    Tutorial on ES6 Classes

--- 14-Object-Oriented-Programming-OOP-with-JS/script.js
+++ 14-Object-Oriented-Programming-OOP-with-JS/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // Lesson: Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
   // Instance properties
@@ -79,3 +80,41 @@ const h1 = document.querySelector('h1');
 console.dir(h1);
 
 console.dir(x => x + 1);
+*/
+
+// Lesson: E6 Classes
+
+// - class expression
+// - const PersonCl = class {}
+
+// - class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
