84be734ae28d1853bd6872be7f7cdad6fd0ac80d
Ronan Corr <ronan_corr@hotmail.com>
2023-09-01 12:17:30 -0400

    coding challenge Part 14 challenge #3 my solution

--- 14-Object-Oriented-Programming/challenges.js
+++ 14-Object-Oriented-Programming/challenges.js
@@ -75,8 +75,10 @@ by 1.6)
 converts it to km/h before storing the value, by multiplying the input by 1.6)
 4. Create a new car and experiment with the 'accelerate' and 'brake'
 methods, and with the getter and setter.
+
 Test data:
 § Data car 1: 'Ford' going at 120 km/h
+
 GOOD LUCK 😀
 */
 
@@ -112,3 +114,63 @@ ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
 console.log(ford);
+
+/*
+Coding Challenge #3
+
+Your tasks:
+1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+"class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+current battery charge in % ('charge' property)
+2. Implement a 'chargeBattery' method which takes an argument
+'chargeTo' and sets the battery charge to 'chargeTo'
+3. Implement an 'accelerate' method that will increase the car's speed by 20,
+and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
+km/h, with a charge of 22%'
+4. Create an electric car object and experiment with calling 'accelerate',
+'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
+you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+
+Test data:
+§ Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+
+*/
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// link the prototype
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const telsa = new EV('Telsa', 120, 23);
+
+console.log(telsa);
+telsa.chargeBattery(90);
+console.log(telsa);
+
+telsa.accelerate();
+telsa.brake();
+telsa.accelerate();
+telsa.accelerate();
+telsa.accelerate();
+telsa.brake();
+telsa.brake();
+telsa.brake();
+telsa.brake();
+telsa.accelerate();
