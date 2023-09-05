5058d35004e9f165f17ff680567d1742c028d1e7
Ronan Corr <ronan_corr@hotmail.com>
2023-09-05 11:15:00 -0400

    Another Class Example tutorial

--- 14-Object-Oriented-Programming-OOP-with-JS/script.js
+++ 14-Object-Oriented-Programming-OOP-with-JS/script.js
@@ -237,7 +237,7 @@ console.log(mike instanceof Object); // true
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
 */
-
+/*
 // Lesson: Inheritance Between "Classes": ES6 Classes
 class PersonCl {
   constructor(fullName, birthYear) {
@@ -303,7 +303,9 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 
 martha.introduce();
 martha.calcAge();
+*/
 
+/*
 // Lesson: Inheritance Between "Classes": Object.create
 const PersonProto = {
   calcAge() {
@@ -332,3 +334,52 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+// Lesson: Another Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface (API)
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  widthdrawal(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// a bad practice
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+// a better way
+acc1.deposit(250);
+acc1.widthdrawal(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
