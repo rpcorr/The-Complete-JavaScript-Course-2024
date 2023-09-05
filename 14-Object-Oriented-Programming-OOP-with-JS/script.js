2893fd8c92dd786ddf4d01be0746cc4f1dee61eb
Ronan Corr <ronan_corr@hotmail.com>
2023-09-05 11:33:02 -0400

    Encapsulation: Protected Properties and Methods tutorial

--- 14-Object-Oriented-Programming-OOP-with-JS/script.js
+++ 14-Object-Oriented-Programming-OOP-with-JS/script.js
@@ -336,6 +336,7 @@ jay.introduce();
 jay.calcAge();
 */
 
+/*
 // Lesson: Another Class Example
 class Account {
   constructor(owner, currency, pin) {
@@ -380,6 +381,58 @@ acc1.deposit(250);
 acc1.widthdrawal(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
+
+*/
+
+// Lesson: Encapsulation: Protected Properties and Methods
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected Property
+    this._pin = pin;
+    this._movements = [];
+
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface (API)
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  widthdrawal(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+acc1.deposit(250);
+acc1.widthdrawal(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
