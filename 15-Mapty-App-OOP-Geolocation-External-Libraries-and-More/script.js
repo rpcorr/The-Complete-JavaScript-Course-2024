8a3988aa3df5046d890d192416236f61ff231c40
Ronan Corr <ronan_corr@hotmail.com>
2023-09-17 22:43:42 -0400

    challenges to do to improve the Mapty app

--- 15-Mapty-App-OOP-Geolocation-External-Libraries-and-More/script.js
+++ 15-Mapty-App-OOP-Geolocation-External-Libraries-and-More/script.js
@@ -323,3 +323,22 @@ class App {
 }
 
 const app = new App();
+
+// TO DO:
+/*
+  EASY
+  1. Ability to edit a workout
+  2. Ability to delete a workout
+  3. Ability to delete all workouts
+  4. Ability to sort workouts by a certain field (e.g. distance). Take a look at Bankist
+  5. Re-build Running and Cycling objects coming from local storage
+  6. More realistic error and confirmation messages. e.g. avoid using alert messages
+
+  HARDER
+  7. Position Map to show all the workouts (very hard)
+  8. Ability to draw lines and shapes instead of just points (very hard)
+
+  ONLY AFTER ASYNCHRONOUS JAVASCRIPT section
+  9. Geocode location from coordinates ("Run in Faro, Portugal")
+  10. Display weather data for workout time and place 
+*/
