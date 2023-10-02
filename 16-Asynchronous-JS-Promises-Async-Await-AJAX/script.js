127b7137fb6bfdd326815af368e8011e9f61166d
Ronan Corr <ronan_corr@hotmail.com>
2023-10-02 19:21:54 -0400

    get neighbouring country data. this introduce callback hell tutorial

--- 16-Asynchronous-JS-Promises-Async-Await-AJAX/script.js
+++ 16-Asynchronous-JS-Promises-Async-Await-AJAX/script.js
@@ -4,6 +4,9 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+
+LESSON: OUR First AJAX Call: XMLHttpRequest
 
 // old school way of making a AJAX call
 const getCountryData = function (country) {
@@ -49,3 +52,91 @@ const getCountryData = function (country) {
 getCountryData('canada');
 getCountryData('portugal');
 getCountryData('usa');
+
+ */
+
+// LESSON: Welcome to Callback
+
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" alt="${
+    data.flags.alt
+  }" />
+          <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+              }</p>
+          </div>
+      </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+  request.send();
+  // console.log(this.responseText); does not work
+
+  request.addEventListener('load', function () {
+    // convert JSON to an object
+    const [data] = JSON.parse(this.responseText);
+
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    const [neighbour] = data.borders;
+
+    // return if no neighbouring countries exists (islands)
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+    );
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('portugal');
+
+// example of call back hell
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
