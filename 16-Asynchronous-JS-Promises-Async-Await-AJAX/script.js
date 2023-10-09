7b6547a3772e7c09d24e1944d188d7ee73aefbfd
Ronan Corr <ronan_corr@hotmail.com>
2023-10-09 10:20:24 -0400

    Running Promises in Parallel tutorial

--- 16-Asynchronous-JS-Promises-Async-Await-AJAX/script.js
+++ 16-Asynchronous-JS-Promises-Async-Await-AJAX/script.js
@@ -3,6 +3,14 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 const renderCountry = function (data, className = '') {
   const html = `
         <article class="country ${className}">
@@ -257,14 +265,6 @@ btn.addEventListener('click', function () {
 /*
 
 // Lesson: Throwing Errors Manually
-
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-
-    return response.json();
-  });
-};
 
 // const getCountryData = function (country) {
 //   // Country 1
@@ -552,6 +552,7 @@ const whereAmI = async function () {
 whereAmI();
 */
 
+/*
 // Lesson: Returning values from Async Functions
 
 const getPosition = function () {
@@ -611,3 +612,35 @@ console.log('1: Will get location');
 
   console.log('3: Finishing getting location');
 })();
+*/
+
+// Lesson: Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://countries-api-836d.onrender.com/countries/name/${c1}`
+    // );
+
+    // const [data2] = await getJSON(
+    //   `https://countries-api-836d.onrender.com/countries/name/${c2}`
+    // );
+
+    // const [data3] = await getJSON(
+    //   `https://countries-api-836d.onrender.com/countries/name/${c3}`
+    // );
+
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.err(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania ');
