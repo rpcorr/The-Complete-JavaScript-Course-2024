cc28433d2f81a5e9f294c8866470a2f087d1123e
Ronan Corr <ronan_corr@hotmail.com>
2023-10-15 09:40:22 -0400

    Other Promises - race, allSettled, and any tutorial

--- 16-Asynchronous-JS-Promises-Async-Await-AJAX/script.js
+++ 16-Asynchronous-JS-Promises-Async-Await-AJAX/script.js
@@ -614,6 +614,8 @@ console.log('1: Will get location');
 })();
 */
 
+/*
+
 // Lesson: Running Promises in Parallel
 const get3Countries = async function (c1, c2, c3) {
   try {
@@ -644,3 +646,57 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'canada', 'tanzania ');
+
+*/
+
+// LESSON: Other Promise Combinators: race, allSettled and any
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://countries-api-836d.onrender.com/countries/name/italy`),
+    getJSON(`https://countries-api-836d.onrender.com/countries/name/egypt`),
+    getJSON(`https://countries-api-836d.onrender.com/countries/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+// Timeout Promise
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://countries-api-836d.onrender.com/countries/name/canada`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve(' Another Success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any [ES2021]
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve(' Another Success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
