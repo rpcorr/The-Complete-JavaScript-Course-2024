1efb91b27b8ce09e6c86a54ed9839b63c8a472a2
Ronan Corr <ronan_corr@hotmail.com>
2023-11-05 06:35:31 -0500

    Top-Level await (ES2022) tutorial

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -18,3 +18,26 @@ add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 console.log(cart);
+
+// console.log('Starting fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+// Using Top-Level Await
+const lastPostTopLevelAwait = await getLastPost();
+console.log(lastPostTopLevelAwait);
