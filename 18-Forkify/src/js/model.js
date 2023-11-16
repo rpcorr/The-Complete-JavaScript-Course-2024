a734cf0f6a3b022e1ff71e8dbfe39f1372d68672
Ronan Corr <ronan_corr@hotmail.com>
2023-11-15 19:05:07 -0500

    add back in state.search.page  = page;

--- 18-Forkify/src/js/model.js
+++ 18-Forkify/src/js/model.js
@@ -56,6 +56,8 @@ export const loadSearchResults = async function (query) {
 };
 
 export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
   const start = (page - 1) * state.search.resultsPerPage; // 0;
   const end = page * state.search.resultsPerPage; // 9;
 
