129b5122da1323c458668d210c20d077f7d5b8bd
Ronan Corr <ronan_corr@hotmail.com>
2023-11-20 09:25:01 -0500

    developing a DOM updating algorithm

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -17,13 +17,15 @@ const controlRecipes = async function () {
     const id = window.location.hash.slice(1);
 
     if (!id) return;
-
     recipeView.renderSpinner();
 
-    // 2) Loading recipe
+    // 2) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    // 3) Loading recipe
     await model.loadRecipe(id);
 
-    // 3) Rendering recipe
+    // 4) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
@@ -64,7 +66,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  //recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
