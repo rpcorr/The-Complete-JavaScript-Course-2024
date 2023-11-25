e08fc22b5a25bbfc79e8d479fa1cbffdac1b1a52
Ronan Corr <ronan_corr@hotmail.com>
2023-11-25 10:52:55 -0500

    uploading a new recipe - part 2

--- 18-Forkify/src/js/config.js
+++ 18-Forkify/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SECS = 10;
 export const RECIPES_PER_PAGE = 10;
+export const API_KEY = '18c4ac6a-a098-43b2-ac4c-c3bbb229186a';
+export const MODAL_CLOSE_SECS = 2.5;
