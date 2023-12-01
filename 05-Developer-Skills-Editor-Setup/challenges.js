74087cf60f93b36ec9ceaff0569479785d6c37d8
Ronan Corr <ronan_corr@hotmail.com>
2023-12-01 10:22:51 -0500

    coding challenge Part 5 challenge #1 my solution

---
+++ 05-Developer-Skills-Editor-Setup/challenges.js
@@ -0,0 +1,34 @@
+/*
+Coding Challenge #1
+
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+
+GOOD LUCK 😀
+
+*/
+
+const temps1 = [17, 21, 23];
+const temps2 = [12, 5, -5, 0, 4];
+
+function printForecast(temp, index) {
+  console.log(`... ${temp}ͦ C in ${index + 1} days ...`);
+}
+
+console.log('Data 1:');
+temps1.forEach((temp, index) => printForecast(temp, index));
+
+console.log('\nData 2:');
+temps2.forEach((temp, index) => printForecast(temp, index));
